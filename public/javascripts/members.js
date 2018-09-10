5e2bb9db3bfdd2afc0fe0f629a76859c03414267
Don <e_fidon@yahoo.com>
2018-09-11 00:19:27 +0100

    update6

---
+++ public/javascripts/members.js
@@ -0,0 +1,8 @@
+var header = document.getElementById('header')
+
+if (n <= 0) {
+	header.innerHTML = 'There are currently no members in this group'
+}
+else {
+	header.innerHTML = 'Group Members'
+}
